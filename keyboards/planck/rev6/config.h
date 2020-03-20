9f37cc2283cf849d6d0a7ba80f617d28f5b8cb58
chax <mcakaric@gmail.com>
2020-03-20 01:41:50 +0100

    planck: rev6: reconfigure matrix for black frankenstein

--- keyboards/planck/rev6/config.h
+++ keyboards/planck/rev6/config.h
@@ -41,7 +41,7 @@
 #undef MATRIX_ROW_PINS
 #undef MATRIX_COL_PINS
 
-#define MATRIX_ROW_PINS { A10, A9, A8, B15, C13, C14, C15, A2 }
+#define MATRIX_ROW_PINS { A10, A9, A8, B15, C13, C14, A14, A2 }
 #define MATRIX_COL_PINS { B11, B10, B2, B1, A7, B0 }
 
 #define UNUSED_PINS
